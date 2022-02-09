6ccf85a46909205b4a9c86532614954082d203df
atticusmatticus <20863400+atticusmatticus@users.noreply.github.com>
2022-02-09 15:00:00 -0600

    added ignore_install for norg because it kept throwing errors when opening nvim.

--- lua/nvim-treesitter.lua
+++ lua/nvim-treesitter.lua
@@ -1,11 +1,14 @@
 -- Enable supported features
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained",  -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  highlight = {
-    enable = true       -- false will disable the whole extension
-  },
-  indent = {
-    enable = true       -- treesitter indentation
-  }
+    ensure_installed = "maintained",  -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    highlight = {
+        enable = true       -- false will disable the whole extension
+    },
+    indent = {
+        enable = true       -- treesitter indentation
+    },
+    ignore_install = {
+        "norg"
+    }
 }
 
